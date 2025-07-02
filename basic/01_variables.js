cb0945943da1c293ea94208b3cc39b06549edf25
lucky-pradip <sahoopradip2005@gmail.com>
2025-07-02 14:28:27 +0000

    data_Type

---
+++ basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12772
+let accountEmail = "pradip500@gmail.com"
+var accountPassword="1234"
+accountCity =" pagalpur"
+
+// accountId=2
+
+accountEmail="hjgdj@.com"
+accountPassword="455"
+accountCity="kolkata"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+//alart(3+3)//we are using nodejs,not browser
+//code readability
+
+console.log(3 + 3); 
+
+let name = "Pradip";
+let score = 3.5;
+let isLoggedIn = false;
+
+//number =>2 to pow53
+//bigint
+//string=>""
+//boolean => true/false
+//null => empty value
+//undefined => variable not defined
+//Symbol => unique
+//object => key value pair
+
+console.log(typeof score); //number
+console.log(typeof name); //string
+console.log(typeof null); //object (this is a bug in JS)
