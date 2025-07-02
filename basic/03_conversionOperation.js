535852d836c68299af0070ee4dbeede857465371
lucky-pradip <sahoopradip2005@gmail.com>
2025-07-02 20:32:44 +0000

    conversionOperation

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "40jhgs";
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber =Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)//for null => 0
+
+
+
+//"33"=>33
+//"33ddaa"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn= "Pradip";
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
